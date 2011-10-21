961a8270862da49082bc0e13242e25bdabf4d240
Phillip LeBlanc <pmleblanc007@gmail.com>
2011-10-20 19:23:01 -0500

    Adding the vim c template file

---
+++ tmpl.c
@@ -0,0 +1,27 @@
+/*
+ * Author: Phillip LeBlanc
+ * Problem:
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define DEBUG 0
+#define printd if (DEBUG) printf
+
+void run()
+{
+  /*do stuff*/
+}
+
+int main()
+{
+  int count,j;
+  scanf("%d", &count);
+  for (j=0;j<count;j++)
+  {
+    run();
+  }
+  return 0;
+}
