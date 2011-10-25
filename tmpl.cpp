82abed153191d29f9e9dc799c31735c8057c7308
Phillip LeBlanc <pmleblanc007@gmail.com>
2011-10-25 14:46:40 -0500

    Adding C++ Template file

---
+++ tmpl.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void run() {
+  
+}
+
+int main(void)
+{
+  int count;
+  cin >> count;
+  for(int i = 0; i < count; i++) {
+    run();
+  }
+}
